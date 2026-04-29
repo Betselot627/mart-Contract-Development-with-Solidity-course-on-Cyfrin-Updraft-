a622b2ab8c149eef81f4f2394b74af2689940c69
ekram-web <unuse2001@gmail.com>
2026-04-29 16:24:00 -0300

    section1:from 1to7

---
+++ section1-simple-storage/code/lesson5-SimpleStorage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// This is my first Solidity smart contract
+// Lesson 5: Setting Up Your First Contract
+
+contract SimpleStorage {
+    // This will get initialized to 0
+    uint256 myFavoriteNumber;
+}
