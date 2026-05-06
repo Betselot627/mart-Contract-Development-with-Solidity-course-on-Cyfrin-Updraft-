73d1624d98209e672414bf75744ba495a6e9d909
suze-ad <suzeadem2@gmail.com>
2026-05-06 23:15:23 +0300

    lesson 18 and 19

--- Section 3 Fund Me Part 1 (lessons 1–15)/code/FundMe.sol
+++ Section 3 Fund Me Part 1 (lessons 1–15)/code/FundMe.sol
@@ -6,6 +6,9 @@ import {PriceConverter} from "./PriceConverter.sol";
 contract FundMe {
     using PriceConverter for uint256;
 
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
     uint256 public minimumUsd = 5e18;
 
     function fund() public payable {
@@ -13,5 +16,25 @@ contract FundMe {
             msg.value.getConversionRate() >= minimumUsd,
             "Didn't send enough ETH"
         );
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
-}
+
+    function withdraw() public {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // Reset the array
+        funders = new address[](0);
+
+        // Withdraw the funds
+        // transfer
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
+
